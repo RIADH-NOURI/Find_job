4d7ec5220ea0f2c4777de2661afb6b4b7d27abaf
RIADH-NOURI <riadhnouri0502@gmail.com>
2025-03-15 18:51:10 +0100

    add keep up server logic

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,21 @@ import dotenv from "dotenv";
 import routesApp from "./app.route";
 import expressConfig from "./config/express.config";
 import helemtSecretConfig from "./config/helmet.secrute.config"
+import axios from "axios";
 dotenv.config();
 
 const app = express();
 const PORT = 5000;
-
+//for testing purposes and no sleep server
+setInterval(async()=>{
+  try{
+         await axios.get('https://findjob-4vl9.onrender.com')
+         console.log('Success response');
+  }
+  catch(error){
+        console.log('error for get data:',error)
+  }
+},10*60*1000);
 
 expressConfig(app);
 
